0c528acff6fab7d838790af9e2a1b5a0cc5c4193
irenezuo20 <qiyao.zuo.gr@dartmouth.edu>
2020-10-25 00:16:03 -0400

    form test

---
+++ form.php
@@ -0,0 +1,18 @@
+<?php
+    $name=$_POST['name'];
+    $visitor_email=$_POST['email'];
+    $message=$_POST['message'];
+
+    $email_from='baekainmybed@163.com';
+    $email_subject="new form submission";
+    $email_body="User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                "User Message: $message.\n";
+
+    $to ="qiyao.zuo.gr@dartmouth.edu";
+    $headers="From: $email_from \r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location:form.html");
+
+
+?>
